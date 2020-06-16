717bbeee40b5296cd9b680369ea22a08740ba6b0
matthewcabor <35939842+matthewcabor@users.noreply.github.com>
2020-06-16 01:55:13 -0500

    Add project files.

---
+++ MC Tech/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MC Tech")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("MC Tech")>
+<Assembly: AssemblyProduct("MC Tech")>
+<Assembly: AssemblyCopyright("Copyright © MC Tech 2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b55e6ad3-d7ff-44f4-8aae-32aeb1bf6e62")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
